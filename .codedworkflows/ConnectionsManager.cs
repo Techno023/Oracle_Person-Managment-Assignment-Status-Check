94b9127d9ff3c8d1c0b28266891912034ef5bd1b
SAP Fiori <sandeepshrivas82@gmail.com>
2025-07-01 15:53:07 +0530

    First Commit

---
+++ .codedworkflows/ConnectionsManager.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+using System;
+
+namespace Oracle_PersonManagmentAssignmentStatusCheck
+{
+    public class ConnectionsManager
+    {
+        public ConnectionsManager(ICodedWorkflowsServiceContainer resolver)
+        {
+        }
+    }
+}
